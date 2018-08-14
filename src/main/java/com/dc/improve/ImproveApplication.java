facb6561a526e6cae4bd8f77e7563d01ee85a9f6
zhangxiaoyang <xiaoyangzhang1990@163.com>
2018-08-14 11:56:56 +0800

    init project

---
+++ src/main/java/com/dc/improve/ImproveApplication.java
@@ -0,0 +1,13 @@
+package com.dc.improve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ImproveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImproveApplication.class, args);
+    }
+}
